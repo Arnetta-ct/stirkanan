e570e7066dd8cf83e1173eb066e0b2d313722c8c
Junywatie - <arnettaspam@gmail.com>
2020-12-26 11:25:56 +0700

    message

--- javascripts/website.js
+++ javascripts/website.js
@@ -1,29 +1,25 @@
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
+function navSlide() {
+  const burger = document.querySelector(".n");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
   
-    // Toggle Nav
-    burger.addEventListener("click", () => {
+  burger.addEventListener("click", () => {
+      //Toggle Nav
       nav.classList.toggle("nav-active");
-  
-      // Animate Links
+      
+      //Animate Links
       navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = "";
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
-            0.5}s`;
-        }
+          if (link.style.animation) {
+              link.style.animation = ""
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+          }
       });
-      // Burger Animation
+      //Burger Animation
       burger.classList.toggle("toggle");
-    });
-  };
+  });
   
-  const app = () => {
-    navSlide();
-  };
-  
-  app();
+}
+
+navSlide();
   
